94e20a26ec53c06ce784e91ea5b92227991c3a3b
Nekomi-CN <78152109+nekomi-cn@users.noreply.github.com>
2022-04-23 17:27:29 +0800

    Update Lede2.sh

--- Lede2.sh
+++ Lede2.sh
@@ -16,6 +16,3 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 # Change Default Theme
 sed -i 's/Bootstrap theme/Argon theme/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
