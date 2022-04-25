9d2268b6cf195b3e5138165ce9d79b00943668d5
Nekomi-CN <78152109+nekomi-cn@users.noreply.github.com>
2022-04-25 20:42:38 +0800

    Update Lede2.sh

--- Lede2.sh
+++ Lede2.sh
@@ -12,3 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+
+# Change Default Theme
+sed -i 's/Bootstrap theme/Argon theme/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
