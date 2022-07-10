187b4ec9f36503981041c7f9f697c9c13ed4b658
zhang <zhangjieepic@gmail.com>
2022-07-10 16:31:24 +0800

    更新编译内容

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # OrangePi R1 Plus LTS Lede云编译脚本
 
-> 使用最新 Lede 源码
+> 使用最新 Lede 源码( WAN口驱动未适配 )
 
-> 使用 Linux 5.10 内核
-
-> 实际测试一切正常，无异常现象
+> 使用 immortalwrt 21.03 分支
 
 每日清晨自动编译，Releases 可下载最新镜像。
--- script/immortalwrt1.sh
+++ script/immortalwrt1.sh
@@ -13,7 +13,6 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i 's/5.*/5.10/g' target/linux/rockchip/Makefile
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
