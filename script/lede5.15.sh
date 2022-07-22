b0be66409d5ad4e3b41a2309d69b97ea988e2515
zhangjieepic@gmail.com <zhangjieepic@gmail.com>
2022-07-22 10:38:16 +0800

    去除不必要的额部分

--- script/lede5.15.sh
+++ script/lede5.15.sh
@@ -13,6 +13,7 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+sed -i 's/5.*/5.15/g' target/linux/rockchip/Makefile
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
