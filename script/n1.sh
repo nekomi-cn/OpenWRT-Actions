ad33a81a68ac6f8fea7e25513c041d7310c9eec1
zhang <zhangjieepic@gmail.com>
2022-07-24 01:19:15 +0800

    添加 N1 编译 和 缓存支持

---
+++ script/n1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+###
+# @Author: iRorikon
+# @Date: 2022-07-10 23:50:30
+# @FilePath: \OrangePi-R1-Plus-LTS-Lede\script\n1.sh
+###
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# fw876/helloworld
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+# Lienol/openwrt-package
+# sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package' feeds.conf.default
