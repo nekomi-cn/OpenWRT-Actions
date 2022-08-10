b7e270681375f49bf9dfb2af9e25ace53e1d2a58
zhangjieepic@gmail.com <zhangjieepic@gmail.com>
2022-08-10 17:26:26 +0800

    回退

--- script/OrangePi2.sh
+++ script/OrangePi2.sh
@@ -19,11 +19,11 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 rm -rf feeds/luci/themes/luci-theme-argon/
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon/
 
-wget --no-check-certificate -O package/boot/uboot-rockchip/Makefile https://raw.githubusercontent.com/nekomi-cn/OpenWRT-Actions/master/patches/rockchip/Makefile
-wget --no-check-certificate -O package/boot/uboot-rockchip/patches/103-tools-disable-TOOLS_LIBCRYPTO.patch https://raw.githubusercontent.com/nekomi-cn/OpenWRT-Actions/master/patches/rockchip/103-tools-disable-TOOLS_LIBCRYPTO.patch
-wget --no-check-certificate -O package/boot/uboot-rockchip/patches/201-scripts-remove-dependency-on-swig.patch https://raw.githubusercontent.com/nekomi-cn/OpenWRT-Actions/master/patches/rockchip/201-scripts-remove-dependency-on-swig.patch
-wget --no-check-certificate -O package/boot/uboot-rockchip/patches/202-spl-remove-dtoc-of-pdata-generation.patch https://raw.githubusercontent.com/nekomi-cn/OpenWRT-Actions/master/patches/rockchip/202-spl-remove-dtoc-of-pdata-generation.patch
+wget --no-check-certificate -O package/boot/uboot-rockchip/Makefile https://raw.githubusercontent.com/nekomi-cn/OpenWRT-Actions/master/patches/Makefile
+wget --no-check-certificate -O package/boot/uboot-rockchip/patches/103-tools-disable-TOOLS_LIBCRYPTO.patch https://raw.githubusercontent.com/coolsnowwolf/lede/53686de196c0827aac3a337a4751b0353f289fcc/package/boot/uboot-rockchip/patches/103-tools-disable-TOOLS_LIBCRYPTO.patch
+wget --no-check-certificate -O package/boot/uboot-rockchip/patches/201-scripts-remove-dependency-on-swig.patch https://raw.githubusercontent.com/coolsnowwolf/lede/53686de196c0827aac3a337a4751b0353f289fcc/package/boot/uboot-rockchip/patches/201-scripts-remove-dependency-on-swig.patch
+wget --no-check-certificate -O package/boot/uboot-rockchip/patches/202-spl-remove-dtoc-of-pdata-generation.patch https://raw.githubusercontent.com/coolsnowwolf/lede/53686de196c0827aac3a337a4751b0353f289fcc/package/boot/uboot-rockchip/patches/202-spl-remove-dtoc-of-pdata-generation.patch
 mkdir -p package/boot/uboot-rockchip/src/of-platdata/orangepi-r1-plus-lts-rk3328
-wget --no-check-certificate -O package/boot/uboot-rockchip/src/of-platdata/orangepi-r1-plus-lts-rk3328/dt-decl.h https://raw.githubusercontent.com/nekomi-cn/OpenWRT-Actions/master/patches/rockchip/of-platdata/dt-decl.h
-wget --no-check-certificate -O package/boot/uboot-rockchip/src/of-platdata/orangepi-r1-plus-lts-rk3328/dt-plat.c https://raw.githubusercontent.com/nekomi-cn/OpenWRT-Actions/master/patches/rockchip/of-platdata/dt-plat.c
-wget --no-check-certificate -O package/boot/uboot-rockchip/src/of-platdata/orangepi-r1-plus-lts-rk3328/dt-structs-gen.h https://raw.githubusercontent.com/nekomi-cn/OpenWRT-Actions/master/patches/rockchip/of-platdata/dt-structs-gen.h
+wget --no-check-certificate -O package/boot/uboot-rockchip/src/of-platdata/orangepi-r1-plus-lts-rk3328/dt-decl.h https://raw.githubusercontent.com/coolsnowwolf/lede/53686de196c0827aac3a337a4751b0353f289fcc/package/boot/uboot-rockchip/src/of-platdata/orangepi-r1-plus-lts-rk3328/dt-decl.h
+wget --no-check-certificate -O package/boot/uboot-rockchip/src/of-platdata/orangepi-r1-plus-lts-rk3328/dt-plat.c https://raw.githubusercontent.com/coolsnowwolf/lede/53686de196c0827aac3a337a4751b0353f289fcc/package/boot/uboot-rockchip/src/of-platdata/orangepi-r1-plus-lts-rk3328/dt-plat.c
+wget --no-check-certificate -O package/boot/uboot-rockchip/src/of-platdata/orangepi-r1-plus-lts-rk3328/dt-structs-gen.h https://raw.githubusercontent.com/coolsnowwolf/lede/53686de196c0827aac3a337a4751b0353f289fcc/package/boot/uboot-rockchip/src/of-platdata/orangepi-r1-plus-lts-rk3328/dt-structs-gen.h
